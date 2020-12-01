36040641b20c84d9620bac37f1393ced186bd6f9
LegendAVRS <61302586+LegendAVRS@users.noreply.github.com>
2020-12-01 15:24:18 +0700

    Added working clock hands

--- Clock/Form1.Designer.cs
+++ Clock/Form1.Designer.cs
@@ -28,21 +28,83 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.numHour = new System.Windows.Forms.NumericUpDown();
+            this.numSec = new System.Windows.Forms.NumericUpDown();
+            this.numMin = new System.Windows.Forms.NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)(this.numHour)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numSec)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numMin)).BeginInit();
             this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 424);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(46, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // numHour
+            // 
+            this.numHour.Location = new System.Drawing.Point(12, 360);
+            this.numHour.Name = "numHour";
+            this.numHour.Size = new System.Drawing.Size(87, 22);
+            this.numHour.TabIndex = 1;
+            this.numHour.ValueChanged += new System.EventHandler(this.numHour_ValueChanged);
+            // 
+            // numSec
+            // 
+            this.numSec.Location = new System.Drawing.Point(231, 360);
+            this.numSec.Name = "numSec";
+            this.numSec.Size = new System.Drawing.Size(87, 22);
+            this.numSec.TabIndex = 2;
+            this.numSec.ValueChanged += new System.EventHandler(this.numSec_ValueChanged);
+            // 
+            // numMin
+            // 
+            this.numMin.Location = new System.Drawing.Point(117, 360);
+            this.numMin.Name = "numMin";
+            this.numMin.Size = new System.Drawing.Size(87, 22);
+            this.numMin.TabIndex = 3;
+            this.numMin.ValueChanged += new System.EventHandler(this.numMin_ValueChanged);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.numMin);
+            this.Controls.Add(this.numSec);
+            this.Controls.Add(this.numHour);
+            this.Controls.Add(this.label1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            ((System.ComponentModel.ISupportInitialize)(this.numHour)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numSec)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numMin)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.NumericUpDown numHour;
+        private System.Windows.Forms.NumericUpDown numSec;
+        private System.Windows.Forms.NumericUpDown numMin;
     }
 }
 
--- Clock/Form1.cs
+++ Clock/Form1.cs
@@ -15,11 +15,100 @@ namespace Clock
         private Graphics g;
         private Pen mypen;
         private Point clock_p = new Point(200, 50);
-        private Point hour_hand_p1, minute_hand_p1, second_hand_p1;
-        private Point hour_hand_p2, minute_hand_p2, second_hand_p2;
+        private Point hour_hand_p, minute_hand_p, second_hand_p, middle_point, new_point;
+        private Point pivot_second, pivot_minute, pivot_hour;
+        private int degree = 6;
+
+        private int cnt_sec = 0, cnt_min = 0, cnt_hour = 0;
+        private bool flag = false;
+
+        private void numMin_ValueChanged(object sender, EventArgs e)
+        {
+            cnt_min = (int)numMin.Value;
+        }
+
+        private void numHour_ValueChanged(object sender, EventArgs e)
+        {
+            cnt_hour = (int)numHour.Value * 5;
+        }
+
+        private void numSec_ValueChanged(object sender, EventArgs e)
+        {
+            cnt_sec = (int)numSec.Value;
+        }
+
         public Form1()
         {
             InitializeComponent();
+            middle_point = clock_p;
+            middle_point.X += 100;
+            middle_point.Y += 100;
+
+            second_hand_p = middle_point;
+            second_hand_p.Y -= 85;
+
+            minute_hand_p = middle_point;
+            minute_hand_p.Y -= 75;
+
+            hour_hand_p = middle_point;
+            hour_hand_p.Y -= 60;
+
+            pivot_second = second_hand_p;
+            pivot_minute = minute_hand_p;
+            pivot_hour = hour_hand_p;
+        }
+
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            label1.Text = e.X.ToString() + " " + e.Y.ToString();
+        }
+
+        void drawHand(ref Point clock_hand, Point pivot, int cnt)
+        {    
+            mypen.Color = this.BackColor;
+            g.DrawLine(mypen, middle_point, clock_hand); //Remove old line
+            
+
+            double rad = -(Math.PI / 180) * degree * cnt;
+
+            Point temp_p = pivot;     //Pivot point
+            temp_p.Y *= -1;
+            temp_p.X -= middle_point.X;
+            temp_p.Y += middle_point.Y;
+
+            clock_hand.X = (int)Math.Ceiling(temp_p.X * Math.Cos(rad)) - (int)Math.Ceiling(temp_p.Y * Math.Sin(rad));
+            clock_hand.Y = (int)Math.Ceiling(temp_p.X * Math.Sin(rad)) + (int)Math.Ceiling(temp_p.Y * Math.Cos(rad));
+
+            clock_hand.X += middle_point.X;
+            clock_hand.Y -= middle_point.Y;
+            clock_hand.Y *= -1;
+
+            mypen.Color = Color.Black;
+            g.DrawLine(mypen, middle_point, clock_hand);
+            
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            ++cnt_sec;
+            drawHand(ref second_hand_p, pivot_second, cnt_sec);
+            drawHand(ref minute_hand_p, pivot_minute, cnt_min);
+            drawHand(ref hour_hand_p, pivot_hour, cnt_hour);
+            if (cnt_sec == 60)
+            {
+                cnt_sec = 1;
+                ++cnt_min;
+                flag = false;
+            }
+            if (cnt_min % 12 == 0 && cnt_min != 0 && !flag)
+            {
+                flag = true;
+                if (cnt_min == 60)
+                {
+                    cnt_min = 0;
+                }
+                ++cnt_hour;
+            }
         }
 
         void drawClock()
@@ -30,17 +119,14 @@ namespace Clock
             g.DrawString("6", new Font("Ariel", 14), Brushes.Black, clock_p.X + 90, clock_p.Y + 175);
             g.DrawString("9", new Font("Ariel", 14), Brushes.Black, clock_p.X + 5, clock_p.Y + 85);
 
-            
-
+            mypen.Color = Color.Black;   
         }
 
         private void Form1_Paint(object sender, PaintEventArgs e)
         {
             g = this.CreateGraphics();
             mypen = new Pen(Color.Black);
-
             drawClock();
-            
         }
     }
 }
